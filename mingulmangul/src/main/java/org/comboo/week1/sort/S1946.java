b73648dd7cf0901f5a8c32cea9b5b2cad5a9bc24
Kwon Min A <min2010011@gmail.com>
2023-09-23 20:17:59 +0900

    refactor: Update 1946
    3272ms -> 876ms

--- mingulmangul/src/main/java/org/comboo/week1/sort/S1946.java
+++ mingulmangul/src/main/java/org/comboo/week1/sort/S1946.java
@@ -3,12 +3,13 @@ package org.comboo.week1.sort;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 /*
  * 1946번 신입 사원 : https://www.acmicpc.net/problem/1946
+ * 코드 개선
+ *  - 동석차가 없기 때문에 순위를 인덱스로 사용 가능!!
+ *   => 정렬할 필요가 없어져서 시간 단축^^ (3272ms -> 876ms)
  */
 public class S1946 {
 
@@ -17,31 +18,30 @@ public class S1946 {
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[][] score = inputScore(br, n);
+            int[] score = inputScore(br, n);
             System.out.println(recruit(score));
         }
     }
 
-    private static int[][] inputScore(BufferedReader br, int n) throws IOException {
-        int[][] score = new int[n][2];
+    private static int[] inputScore(BufferedReader br, int n) throws IOException {
+        int[] score = new int[n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            score[i][0] = Integer.parseInt(st.nextToken());
-            score[i][1] = Integer.parseInt(st.nextToken());
+            int idx = Integer.parseInt(st.nextToken()) - 1;
+            int value = Integer.parseInt(st.nextToken());
+            score[idx] = value;
         }
         return score;
     }
 
-    private static int recruit(int[][] score) {
-        Arrays.sort(score, Comparator.comparingInt(e -> e[0]));
-
+    private static int recruit(int[] score) {
         int pick = 1;
-        int lastRank = score[0][1];
+        int lastRank = score[0];
 
         for (int i = 1; i < score.length; i++) {
-            if (score[i][1] < lastRank) {
+            if (score[i] < lastRank) {
                 pick++;
-                lastRank = score[i][1];
+                lastRank = score[i];
             }
             if (lastRank == 1) {
                 break;
