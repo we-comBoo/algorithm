4b82516f8dff9528021b9caba92a2d0cc2cbe919
Kwon Min A <min2010011@gmail.com>
2023-10-01 18:37:31 +0900

    feat: solve 4889
    백준 4889번: 안정적인 문자열

---
+++ mingulmangul/src/main/java/org/comboo/week2/mapsetqueue/S4889.java
@@ -0,0 +1,59 @@
+package org.comboo.week2.mapsetqueue;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/**
+ * 4889번 안정적인 문자열: https://www.acmicpc.net/problem/4889
+ */
+public class S4889 {
+
+    /**
+     * if {
+     * 	  push {
+     * if }
+     * 	if stack is empty
+     *    } -> { 연산 수행 (answer++)
+     *  pop {
+     * ---
+     * if stack is not empty
+     *  answer += stack.size() / 2
+     */
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; ; i++) {
+            String str = br.readLine();
+            if (!str.isEmpty() && str.charAt(0) == '-') {
+                break;
+            }
+            int answer = calcAnswer(str);
+            System.out.println(i + ". " + answer);
+        }
+    }
+
+    private static int calcAnswer(String str) {
+        Deque<Character> stack = new ArrayDeque<>();
+        int length = str.length();
+        int answer = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == '{') {
+                stack.push('{');
+            } else if (stack.isEmpty()) {
+                answer++;
+                stack.push('{');
+            } else {
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return answer + (stack.size() / 2);
+        }
+        return answer;
+    }
+
+}
